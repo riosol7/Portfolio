e08a1badab61326ac982df1ddbb772982604cf09
piraiojeda <piraiojeda@gmail.com>
2021-09-11 01:51:27 -0700

    draft1

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+const PORT = 9999
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.render('home.ejs');
+});
+
+
+app.listen(PORT, () => {
+    console.log(PORT)
+})
